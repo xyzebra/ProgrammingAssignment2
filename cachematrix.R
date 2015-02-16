ede431ac32237231906772b57079905b464cd6a4
xyzebra <inaisworking@Inas-MacBook-Air.local>
2015-02-16 21:55:34 +0100

    Revert "Assignment 2 completed"
    This reverts commit 13ae7265b234470b938e839fee5bd264990f5f35.

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,15 @@
-## Caching calculated values rather then computing them each time can save valuable time and computing
-## resorces. Here a matrix is created to cache an input matrix and its inverse.
+## Put comments here that give an overall description of what your
+## functions do
 
-## This function creates a "matrix" where  a computed inverse of that matrix can be stored. 
-## First, default values are set and then the matrix inverse is computed and saved into the "matrix"
-## as a list.
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y){
-    x <<- y
-    m <<- NULL
-  }
-get <- function() x
-setmatrix <- function(solve) m <<- solve
-getmatrix <- function() m
-list(set=set, get=get,
-     setmatrix=setmatrix,
-     getmatrix=getmatrix)
+
 }
 
-## This function checks if an inverse of a matrix has been computed and stored. If it has it gives back
-## the stored (cached) data to gether with a note "getting chaed data", if it has not the inverse is
-## calculated and chached.
+
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  m<-x$getmatrix()
-  if(!is.null(m)){
-    message("getting cached data")
-    return(m)
-  }
-  matrix<-x$get()
-  m<-solve(matrix, ...)
-  x$setmatrix(m)
-  m
         ## Return a matrix that is the inverse of 'x'
 }
